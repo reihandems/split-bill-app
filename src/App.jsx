603fe73490b74e897af0edf5bb9595cddb63038a
rdemstion <179684859+reihandems@users.noreply.github.com>
2026-09-19 23:06:44 +0700

    add: friendlist

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import FriendsData from "./friends.json"
 
+import Friendlist from "./components/Friendlist";
 
 function App() {
-
+  const friends = FriendsData;
+  
   return (
     <>
-      
+      <div className="font-mono flex flex-col items-center justify-center min-h-screen p-8">
+        <div className="text-4xl font-bold mb-5 text-center">Split Bill App</div>
+        <Friendlist friends={friends} />
+      </div>
     </>
   )
 }
