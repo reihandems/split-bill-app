1d5d954e17fe96a937afe6aafe5b8512d73035b9
rdemstion <179684859+reihandems@users.noreply.github.com>
2026-09-22 10:49:42 +0700

    add: empty state in Friends Data List

--- src/components/Friendlist.jsx
+++ src/components/Friendlist.jsx
@@ -1,18 +1,29 @@
 import Friend from "./Friend"
 
 export default function Friendlist({ friends, handleSelect, selected, handleSplitBill, onDeleteItem }) {
+  
   return (
     <>
-      {friends.map((friend) => (
-        <Friend
-          friend={friend}
-          key={friend.id}
-          handleSelect={handleSelect}
-          selected={selected}
-          handleSplitBill={handleSplitBill}
-          onDeleteItem={onDeleteItem}
-        />
-      ))}
+      {friends.length === 0 ? (
+        <div className="flex gap-4 my-2 bg-zinc-900 p-6 rounded-xl items-center justify-between w-auto sm:w-full flex-wrap">
+          No data available.
+          <br />{" "}
+          <span className="text-gray-600">
+            Add new friend to start split-bill
+          </span>
+        </div>
+      ) : (
+        friends.map((friend) => (
+          <Friend
+            friend={friend}
+            key={friend.id}
+            handleSelect={handleSelect}
+            selected={selected}
+            handleSplitBill={handleSplitBill}
+            onDeleteItem={onDeleteItem}
+          />
+        ))
+      )}
     </>
   );
 }
